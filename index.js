6fd9c6db7cf89789e0ea0269fd70bd47c643f9fd
Peter Dodds <peter@pddds.com>
2017-11-18 19:06:56 +0000

    Add fontawesome

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static("node_modules/font-awesome"));
 app.listen(3000, () => console.log("Listening on port 3000"));
