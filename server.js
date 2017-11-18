709f556aea0ac61e81ef5b8d3524e58951a7896e
Peter Dodds <peter@pddds.com>
2017-11-18 19:39:19 +0000

    Basic vue ssr

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
+const Vue = require('vue')
+const renderer = require('vue-server-renderer').createRenderer()
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/font-awesome"));
+
+app.get('/', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
+})
+
 app.listen(3000, () => console.log("Listening on port 3000"));
